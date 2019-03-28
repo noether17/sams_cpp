132a97aaa2b2a88d63de2a54d2c9236ae3620310
Jon <jawhite@oakland.edu>
2019-03-27 23:00:14 -0400

    add listing 2.1

---
+++ hello_world_analysis.cpp
@@ -0,0 +1,12 @@
+// Preprocessor directive that includes header iostream
+#include <iostream>
+
+// Start of your program: function block main()
+int main()
+{
+	/* Write to the screen */
+	std::cout << "Hello, world!" << std::endl;
+
+	// return a value to the OS
+	return 0;
+}
