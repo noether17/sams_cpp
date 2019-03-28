dea11a7d63f52c92ae63f0a2b799a29cfff60d7e
Jon <jawhite@oakland.edu>
2019-03-27 23:42:25 -0400

    add listing 2.3

---
+++ list2_3.cpp
@@ -0,0 +1,15 @@
+// Preprocessor directive
+#include <iostream>
+
+// Start of your program
+int main()
+{
+	using std::cout;
+	using std::endl;
+
+	/* Write to the screen using std::cout */
+	cout << "Hello world" << endl;
+
+	// Return a value to the OS
+	return 0;
+}
