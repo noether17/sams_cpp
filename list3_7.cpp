cb91d6748805963ba8ae70ec574d9701d47bc0af
Jon White <jawhite@oakland.edu>
2019-05-04 19:56:48 -0400

    3.7

---
+++ list3_7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	const double pi = 3.14159265358979;
+	cout << "The value of constant pi is: " << pi << endl;
+
+	//Uncomment to cause compile failure
+	//pi = 345;
+	
+	return 0;
+}
