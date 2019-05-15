c11f79317e5955a1d3f026118970f22f70a7ca8c
Jon White <jawhite@oakland.edu>
2019-05-15 16:16:48 -0400

    7.1

---
+++ list7_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+const double PI = 3.14159265;
+
+// Function declarations
+double Area(double radius);
+double Circumference(double radius);
+
+int main()
+{
+	cout << "Enter radius: ";
+	double radius = 0;
+	cin >> radius;
+
+	// Call function "Area"
+	cout << "Area is: " << Area(radius) << endl;
+
+	// Call function "Circumference"
+	cout << "Circumference: " << Circumference(radius) << endl;
+
+	return 0;
+}
+
+// Function definitions
+double Area(double radius)
+{
+	return PI * radius * radius;
+}
+
+double Circumference(double radius)
+{
+	return 2 * PI * radius;
+}
