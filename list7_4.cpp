a689b80ebb9a725961eef3af30c3b63b8c4e86f4
Jon White <jawhite@oakland.edu>
2019-05-22 17:29:08 -0400

    through 7.4

---
+++ list7_4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// function declarations (prototypes)
+double Area(double radius, double pi = 3.14);
+
+int main()
+{
+	cout << "Enter radius: ";
+	double radius = 0;
+	cin >> radius;
+
+	cout << "pi is 3.14, do you wish to change this (y / n)? ";
+	char changePi = 'n';
+	cin >> changePi;
+
+	double circleArea = 0;
+	if (changePi == 'y')
+	{
+		cout << "Enter new pi: ";
+		double newPi = 3.14;
+		cin >> newPi;
+		circleArea = Area(radius, newPi);
+	}
+	else
+		circleArea = Area(radius);
+
+	cout << "Area is: " << circleArea << endl;
+
+	return 0;
+}
+
+double Area(double radius, double pi)
+{
+	return pi * radius * radius;
+}
